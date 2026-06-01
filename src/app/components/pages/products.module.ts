e6004b5681f0a68aa90784f9a1a435ece168ec89
Terekhov Ivan <for-spam32@yandex.ru>
2026-06-01 11:21:38 +0400

    done: компоненты касательно продуктов вынесены в отдельный модуль

---
+++ src/app/components/pages/products.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ProductComponent} from "./product/product.component";
+import {ProductsComponent} from "./products/products.component";
+import {ProductCardComponent} from "../common/product-card/product-card.component";
+import {TitleComponent} from "../common/title/title.component";
+import {RouterModule} from "@angular/router";
+import {WordUpperPipe} from "../../pipes/word-upper.pipe";
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    ProductsComponent,
+    ProductCardComponent,
+    TitleComponent,
+    WordUpperPipe,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ]
+})
+export class ProductsModule { }
