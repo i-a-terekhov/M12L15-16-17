4a25c406f0633375b5292ec3efbaad4958ef1161
Terekhov Ivan <for-spam32@yandex.ru>
2026-06-02 11:51:30 +0400

    done: бызовые элементы вынесены в отдельный layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LayoutComponent} from "./views/layout.component";
 
 const routes: Routes = [
-
   {
     path: '',
+    component: LayoutComponent,  // это удобно, если мы хотим использовать разные хедеры/футеры, разместив для другого "комплекта" другой layout
     children: [
       {path: '', loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule)},
       {path: 'order', loadChildren: () => import('./views/order/order.module').then(m => m.OrderModule)},
